3834d2d5cdc21942129797b693b87f628df9951d
Robert Lange <roberto.lange@gmail.com>
2021-10-03 14:53:20 +0200

    docs: add comments in code

--- index.js
+++ index.js
@@ -2,32 +2,42 @@ import { useEffect, useRef, useCallback } from 'react';
 
 const noop = () => {};
 
-const useClickOutside = (handler = noop) => {
+const useClickOutside = (fn = noop) => {
     const lastTarget = useRef();
     const containerRef = useRef();
 
     useEffect(() => {
         let timeoutId;
         const clickHandler = (event) => {
+            // This event will be called each time click event is captured in the target owner document.
+            // Because it is attached to the document object, it will be called before the React
+            // onClickCapture event handler, which is attached to the app root element (document's descendant).
+            // That's why check must be postponed - to make sure the React's handler executes first.
             timeoutId = setTimeout(() => {
+                // If event target is different than last react handler target
+                // this muse be an outside click event.
                 if (lastTarget.current !== event.target) {
-                    handler();
+                    fn();
+                    // Cleanup event target to prevent memory leaks.
                     lastTarget.current = null;
                 }
             }, 0);
         };
 
+        // use ownerDocument because node could be portaled into other browser window
         const targetDocument = containerRef.current.ownerDocument;
         targetDocument.addEventListener('click', clickHandler, { capture: true });
 
         return () => {
+            // Clear all things that could be leaked.
             clearTimeout(timeoutId);
             lastTarget.current = null;
             targetDocument.removeEventListener('click', clickHandler, { capture: true });
         };
-    }, [handler]);
+    }, [fn]);
 
     const onClickCapture = useCallback((event) => {
+        // Save event target on click to compare it with target in document dom click handler.
         lastTarget.current = event.target;
     }, []);
 
